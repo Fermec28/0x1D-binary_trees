2da182b3b6546245324009abe7614435d229384d
Jose Medina <fermed28@gmail.com>
2019-07-26 04:08:23 +0000

    rotate left

--- binary_trees.h
+++ binary_trees.h
@@ -53,4 +53,5 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
